b955cfa24e6a6de9d340b509adc17eda20026879
Lê Văn Đôn <mrledon90@hotmail.com>
2021-02-01 20:01:52 +0700

    update dll

---
+++ RSAGenerator/RSAGenerator/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RSAGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool willContinue = true;
+            int menu = -1;
+            while (willContinue)
+            {
+                Console.WriteLine("Enter to select function:");
+                Console.WriteLine("1. Gernate Private/Public RSA256 Key Pair");
+                Console.WriteLine("9. Exit");
+                
+                int.TryParse(Console.ReadLine(), out menu);
+
+                switch (menu)  
+                {
+                    case 1:
+                        //statement 
+                        break;
+
+                    //case 2:
+                    //    //statement 
+                    //    break;
+
+                    //case 3:
+                    //    //statement 
+                    //    break;
+
+                    case 4:
+                    default:
+                        willContinue = false;
+                        break;
+                }
+            }
+        }
+    }
+}
